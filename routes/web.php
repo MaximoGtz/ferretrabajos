99e55f2b8674143dc22fff05dd63766d211b7602
Juan Vega <obijuankenobi1103@gmail.com>
2024-10-17 11:57:53 -0600

    borre algo del web

--- routes/web.php
+++ routes/web.php
@@ -52,8 +52,6 @@ Route::middleware(['auth:administradore'])->group(function () {
 
 
 });
-Route::view('/productos', '/tareapis/catalogo');
-Route::view('/productos/detalle', '/tareapis/detalle');
 
 // admin login
 Route::post('/admin/login', [AdminAuthController::class, 'login']);
