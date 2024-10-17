329d4cfd251f0579def1eae36b7aecf06e198a1f
Maximo <maximogtzog@gmail.com>
2024-10-17 12:09:16 -0600

    Borré un boton del navbar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -130,11 +130,6 @@
                         <a href="/servicios"
                             class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Servicios</a>
                     </li>
-                    <li>
-                        <a href="/catalogo"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tarea
-                            API</a>
-                    </li>
                 </ul>
             </div>
         </div>
